df46f27a879f07d752de812c1e60a20d1e0f5b20
Akshay "XA" Mestry <xa@mes3.dev>
2024-06-23 19:49:19 -0500

    chore: upstream config store to remote
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

---
+++ rias/utils/config.py
@@ -0,0 +1,162 @@
+"""\
+Rias Configuration Management
+============================
+
+Author: XA <xa@mes3.dev>
+Created on: Sunday, June 23 2024
+Last updated on: Sunday, June 23 2024
+
+This module manages all configurations for the Rias framework. It
+provides access to configuration values defined in Rias' defaults module
+and allows for dynamic access and updating of these configurations during
+runtime.
+"""
+
+from __future__ import annotations
+
+import importlib
+import os
+import typing as t
+
+from rias.utils import defaults
+from rias.utils.functional import LazyLoader
+from rias.utils.functional import empty
+
+VT = t.TypeVar("VT")
+
+_USER_CONFIG_MODULE: t.Final[str] = "USER_CONFIG_MODULE"
+
+
+class ConfigurationStore:
+    """A class that manages all Rias' configurations.
+
+    This class provides access to all configurations defined in Rias'
+    defaults module. It allows for dynamic access to configuration
+    values, default values, and provides methods for managing and
+    updating configurations during runtime.
+
+    :var _overridden: Set to track overridden configuration attributes.
+    :param module: Path to the user-defined configuration module.
+
+    .. note::
+
+        The ConfigurationStore class is designed to be used as a
+        singleton, although not completely. The instance of config store
+        is accessed via the LazyConfigurationStore class which wraps
+        this one.
+    """
+
+    def __init__(self, module: str | None) -> None:
+        """Initialize the configuration store with specified configuration
+        module.
+
+        This constructor imports default configuration values from the
+        defaults module but still allowing it to be overridden by the
+        values from a user-specified configuration module if provided.
+        """
+        for default in dir(defaults):
+            setattr(self, default, getattr(defaults, default))
+        self.__dict__["_overridden"] = set()
+        self._user_module = module
+        if self._user_module:
+            settings = importlib.import_module(self._user_module)
+            for setting in dir(settings):
+                setattr(self, setting, getattr(settings, setting))
+
+    def __repr__(self) -> str:
+        """Return a string representation of the configuration store."""
+        if self._user_module:
+            return f"<{self.__class__.__name__}: {self._user_module!r}>"
+        return f"<{self.__class__.__name__}: {defaults.__name__!r}>"
+
+
+class LazyConfigurationStore(LazyLoader):
+    """A lazy proxy class for managing Rias' configurations.
+
+    This class extends the ``LazyLoader`` to provide lazy loading
+    capabilities specifically for configuration management. The purpose of
+    this class is to defer the initialization and loading of the
+    configuration settings until they are actually accessed. This can
+    optimize performance and resource usage by avoiding unnecessary
+    loading of configuration data.
+
+    The configuration is not loaded at the time of the object's creation.
+    Instead, it is loaded only when a configuration value is accessed for
+    the first time. Once the configuration value is accessed and loaded,
+    it is cached within the instance to allow for quick subsequent
+    access without the need to reload the data.
+
+    The class also provides overridden attribute access methods to
+    seamlessly manage the lazy loading and caching of configuration
+    values.
+
+    .. note::
+
+        The LazyConfigurationStore class is designed to provide the same
+        interface as the ConfigurationStore class, forwarding all
+        attribute and method accesses to the underlying instance once it
+        is initialized.
+    """
+
+    def _load(self, name: str | None = None) -> None:
+        """Load the underlying ConfigurationStore instance.
+
+        This method initializes the ConfigurationStore instance and
+        stores it in the _wrapped attribute. It is called automatically
+        when the lazy proxy is accessed for the first time.
+
+        .. note::
+
+            This method must be implemented by subclasses of LazyLoader.
+        """
+        self._wrapped = ConfigurationStore(os.environ.get(_USER_CONFIG_MODULE))
+
+    def __repr__(self) -> str:
+        """Return a string representation of the lazy config store."""
+        if self._wrapped is empty:
+            return f"<LazyConfigurationStore: 'default'>"
+        return repr(self._wrapped)
+
+    def __getattr__(self, name: str) -> t.Any:
+        """Retrieve the value of a configuration and cache it."""
+        if (_wrapped := self._wrapped) is empty:
+            self._load(name)
+            _wrapped = self._wrapped
+        value = getattr(_wrapped, name)
+        self.__dict__[name] = value
+        return value
+
+    def __setattr__(self, name: str, value: t.Any) -> None:
+        """Set the value of a configuration."""
+        if name == "_wrapped":
+            self.__dict__.clear()
+        else:
+            if name not in self.__dict__:
+                raise AttributeError(
+                    "ConfigurationStore is already configured. You must set "
+                    f"the attribute using configuration.update({name}={value})"
+                )
+            self.__dict__.pop(name, None)
+        return super().__setattr__(name, value)
+
+    def __delattr__(self, name: str) -> None:
+        """Delete a configuration value and clear it from cache."""
+        super().__delattr__(name)
+        self.__dict__.pop(name, None)
+
+    def update(self, **kwargs: VT) -> None:
+        """Update and reconfigure store with additional options."""
+        for key, value in kwargs.items():
+            if key in self.__dict__:
+                self._overridden.add(key)
+            else:
+                self.__dict__[key] = object()
+            setattr(self, key, value)
+
+    @property
+    def configured(self) -> bool:
+        """Return True if the values have already been configured."""
+        return self._wrapped is not empty
+
+
+configuration = LazyConfigurationStore()
---
+++ rias/utils/defaults.py
@@ -0,0 +1,15 @@
+"""\
+Rias Default Settings
+=====================
+
+Author: XA <xa@mes3.dev>
+Created on: Sunday, June 23 2024
+Last updated on: Sunday, June 23 2024
+
+This module defines the default settings and values for the Rias
+framework. These defaults are used throughout the framework unless
+overridden by user-defined configurations.
+"""
+
+# TODO (xames3): Enforce value validation and type checking
+debug: bool = True
