86abbca7608099537ccada282661e7f8aa1ac2c6
Akshay "XA" Mestry <xa@mes3.dev>
2024-06-30 13:18:54 -0500

    fix: resolve cyclic import issue
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- rias/__init__.py
+++ rias/__init__.py
@@ -4,7 +4,7 @@ Rias Framework
 
 Author: XA <xa@mes3.dev>
 Created on: Sunday, June 23 2024
-Last updated on: Sunday, June 23 2024
+Last updated on: Saturday, June 29 2024
 
 This module initializes the Rias framework, setting up the necessary
 imports and configurations required for the framework to function
@@ -17,7 +17,11 @@ relies on a community of contributors who write plugins or agents to
 extend its functionality.
 """
 
-from rias.utils.config import configuration as configuration
 from rias.version import __version__ as __version__
 
 version: str = __version__
+
+
+def setup() -> None:
+    """Configure the framework and register the agents."""
+    pass
