bcf943e222d9353ccc781dede67f206d73beda68
Akshay "XA" Mestry <xa@mes3.dev>
2024-07-13 18:28:23 -0500

    docs: minor docstring correction
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ Rias
 
 Author: XA <xa@mes3.dev>
 Created on: Sunday, June 23 2024
-Last updated on: Sunday, June 23 2024
-
-:copyright: (c) 2024 XA. All rights reserved.
-:license: MIT, see LICENSE for more details.
+Last updated on: Thursday, July 11 2024
 """
 
 import platform
